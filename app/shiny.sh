4b346512f150375b4aff2eff925cb33dcf8850a4
mike <barrette@ip24.m>
2019-06-13 15:55:48 -0400

    ajout shiny

---
+++ app/shiny.sh
@@ -0,0 +1,44 @@
+
+############################################################################
+# R-Shiny
+############################################################################
+
+function shiny_create {
+mkdir -p $app_path/.shiny/{lib,log} $app_path/ftp
+
+cat >$app_confdir/shiny-server.conf <<fin666
+# Instruct Shiny Server to run applications as the user "shiny"
+run_as $USER;
+
+# Define a server that listens on port 3838
+server {
+  listen 3838;
+
+  # Define a location at the base URL
+  location / {
+
+    # Host the directory of Shiny Apps stored in this directory
+    site_dir /srv/shiny-server;
+
+    # Log all Shiny output to files in this directory
+    log_dir /var/log/shiny-server;
+
+    # When a user visits the base URL rather than a particular application,
+    # an index of the applications available in this directory will be shown.
+    directory_index on;
+  }
+}
+
+fin666
+}
+
+
+function shiny_start() {
+   SHINY_PORT=$(remote_freeport)
+   sed "s/listen .*/listen $SHINY_PORT;/" -i $app_confdir/shiny-server.conf
+   singularity instance start -B $app_confdir/shiny-server.conf:/etc/shiny-server/shiny-server.conf -B $app_path/.shiny/log:/var/log/shiny-server -B  $app_path/.shiny/lib:/var/lib/shiny-server -B $app_path/ftp:/ftp docker://rocker/shiny $app_id
+   singularity exec instance://$app_id bash -c "nohup shiny-server &"
+
+   export REMOTE_FREE_PORT=$SHINY_PORT
+}
+
